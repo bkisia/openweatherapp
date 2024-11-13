15bccaf93e1fc855d10628680273931062e49dcd
bkisia <137644764+bkisia@users.noreply.github.com>
2024-11-13 17:31:53 +0100

    update

--- src/Components/MainDisplay.js
+++ src/Components/MainDisplay.js
@@ -1,4 +1,4 @@
-// Products.js
+import { API } from '../Config/Config';
 import React, { useRef, useEffect, useState } from 'react';
 import thermometer from '../images/thermometer-mercury-cold.svg';
 import clear from '../images/01d.svg';
@@ -20,109 +20,194 @@ import snown from '../images/13n.svg';
 import mist from '../images/50d.svg';
 import mistn from '../images/50n.svg';
 
-export default function MainDisplay() {
-    const [weather, setWeather] = useState({});
-    const [city, setCity] = useState('Paris');
-    const [units, setUnits] = useState('metric');
-    const searchRef = useRef();
 
-    // Define the mapping of icon IDs to SVG imports
-    const iconMap = {
-        "01d": clear, "01n": clearn,
-        "02d": fewclouds, "02n": fewcloudsn,
-        "03d": scatteredclouds, "03n": scatteredcloudsn,
-        "04d": brokenclouds, "04n": brokencloudsn,
-        "09d": shower, "09n": showern,
-        "10d": rain, "10n": rainn,
-        "11d": thunderstorm, "11n": thunderstormn,
-        "13d": snow, "13n": snown,
-        "50d": mist, "50n": mistn
-    };
 
-    const weatherData = async () => {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&id=524901&units=${units}&appid=3e43c3d8b684e8223c23a336c725a765`);
 
-        const data = await response.json();
-        console.log(data);
-        setWeather(data);
-    };
+    export default function MainDisplay() {
+        const [weather, setWeather] = useState({});
+        const [city, setCity] = useState("");
+        const [units, setUnits] = useState("metric");
+        const searchRef = useRef();
+        const [error, setError] = useState(false);
+        
+        
 
-    const getCurrentLocation = () => {
-        navigator.geolocation.getCurrentPosition(
-            (pos) => console.log(pos),
-            (e) => console.log(e)
-        );
-    };
-    getCurrentLocation();
 
-    useEffect(() => {
-        weatherData();
-    }, [city, units]);
-
-    const search = () => {
-        setCity(searchRef.current.value);
-    };
-
-    const formatDateTime = (unixTime) => {
-        const date = new Date(unixTime * 1000);
-        const formattedDate = date.toLocaleDateString('en-US', {
-            year: 'numeric',
-            month: 'short',
-            day: 'numeric'
-        });
-        const formattedTime = date.toLocaleTimeString('en-US', {
-            hour: '2-digit',
-            minute: '2-digit',
-            hour12: true
-        });
-        return `${formattedDate} ${formattedTime}`;
-    };
-
-    return (
-        <div className='maindisplay-container'>
-            <div className='city-temp-container'>
-                <div className='city-country'>
-                    <img src={`https://flagcdn.com/28x21/${weather.sys?.country?.toLowerCase()}.png`}
-                        width="30" alt="flag"
-                    />
-                    <h1><big className='city-text'>{weather.name}</big></h1>
-                    <small>{weather.sys?.country}</small>
-                    
-                </div>
-                
-                <div className='search-container'>
-                    <input type='text' placeholder='Search City' className='search-bar' ref={searchRef} />
-                    <button className='search-button' onClick={search}>Search</button>
-                </div>
-                <span className='time-temp'>
-                    <h2>{weather.dt ? formatDateTime(weather.dt) : ''}</h2>
-                </span>
-            </div>
-            <div className='weather-details-container'>
-                <div className='weather-icon'>
+        // Define the mapping of icon IDs to SVG imports
+        const iconMap = {
+          "01d": clear,
+          "01n": clearn,
+          "02d": fewclouds,
+          "02n": fewcloudsn,
+          "03d": scatteredclouds,
+          "03n": scatteredcloudsn,
+          "04d": brokenclouds,
+          "04n": brokencloudsn,
+          "09d": shower,
+          "09n": showern,
+          "10d": rain,
+          "10n": rainn,
+          "11d": thunderstorm,
+          "11n": thunderstormn,
+          "13d": snow,
+          "13n": snown,
+          "50d": mist,
+          "50n": mistn,
+        };
+      
+        const weatherData = async () => {
+          const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&id=524901&units=${units}&appid=${API.appIdB}`
+          );
+      
+          const data = await response.json();
+          console.log(data);
+          setWeather(data);
+        };
+      
+        const getCurrentLocation = () => {
+          navigator.geolocation.getCurrentPosition(
+            async (pos) => {
+              console.log(pos);
+              const response = await fetch(
+                `https://api.openweathermap.org/data/2.5/weather?lat=${pos.coords.latitude}&lon=${pos.coords.longitude}&id=524901&units=${units}&appid=${API.appIdB}`
+              );
+              const data = await response.json();
+              console.log(data);
+              setWeather(data);
+      
+              setError(false);
+            },
+            (e) => {
+              console.log(e);
+              setError(true);
+            }
+          );
+        };
+      
+        useEffect(() => {
+          if (city === "") {
+            getCurrentLocation();
+          } else {
+            weatherData();
+          }
+        }, [city, units]);
+      
+        const search = () => {
+          setCity(searchRef.current.value);
+        };
+      
+        const formatDateTime = (unixTime) => {
+          const date = new Date(unixTime * 1000);
+          const formattedDate = date.toLocaleDateString("en-US", {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          });
+          const formattedTime = date.toLocaleTimeString("en-US", {
+            hour: "2-digit",
+            minute: "2-digit",
+            hour12: true,
+          });
+          return `${formattedDate} ${formattedTime}`;
+        };
+      
+        return (
+          <div className="maindisplay-container">
+            {error ? (
+              <>
+                <center>
+                  <h1>Allow location service to see the current weather</h1>
+                </center>
+              </>
+            ) : (
+              <>
+                <div className="city-temp-container">
+                  <div className="city-country">
                     <img
-                        className='icon-image'
-                        src={iconMap[weather.weather?.[0]?.icon] || thermometer}
-                        alt='weather-icon'
+                      src={`https://flagcdn.com/28x21/${weather.sys?.country?.toLowerCase()}.png`}
+                      width="30"
+                      alt="flag"
                     />
+                    <h1>
+                      <big className="city-text">{weather.name}</big>
+                    </h1>
+                    <small>{weather.sys?.country}</small>
+                  </div>
+      
+                  <div className="search-container">
+                    <input
+                      type="text"
+                      placeholder="Search City"
+                      className="search-bar"
+                      ref={searchRef}
+                    />
+                    <button className="search-button" onClick={search}>
+                      Search
+                    </button>
+                  </div>
+                  <span className="time-temp">
+                    <h2>{weather.dt ? formatDateTime(weather.dt) : ""}</h2>
+                  </span>
                 </div>
-                <div className='temperature-container'>
-                    <h2><big className='temperature'>{weather.main ? Math.round(weather.main.temp) : ''}°{units === 'metric' ? 'C' : 'F'}</big></h2>
-                    {units === 'metric'
-                        ? <button className='unit-button' onClick={() => setUnits('imperial')}>°F</button>
-                        : <button className='unit-button' onClick={() => setUnits('metric')}>°C</button>
-                    }
+                <div className="weather-details-container">
+                  <div className="weather-icon">
+                    <img
+                      className="icon-image"
+                      src={iconMap[weather.weather?.[0]?.icon] || thermometer}
+                      alt="weather-icon"
+                    />
+                  </div>
+                  <div className="temperature-container">
+                    <h2>
+                      <big className="temperature">
+                        {weather.main ? Math.round(weather.main.temp) : ""}°
+                        {units === "metric" ? "C" : "F"}
+                      </big>
+                    </h2>
+                    {units === "metric" ? (
+                      <button
+                        className="unit-button"
+                        onClick={() => setUnits("imperial")}
+                      >
+                        °F
+                      </button>
+                    ) : (
+                      <button
+                        className="unit-button"
+                        onClick={() => setUnits("metric")}
+                      >
+                        °C
+                      </button>
+                    )}
+                  </div>
+                  <h2 className="weather-type">
+                    {weather.weather?.[0]?.description}
+                  </h2>
+                  <hr />
+                  <h3 className="weather-description">
+                    Humidity: <b>{weather.main?.humidity}%</b>
+                  </h3>
+                  <hr />
+                  <h3 className="weather-description">
+                    Feels like:{" "}
+                    <b>
+                      {weather.main ? Math.round(weather.main.feels_like) : ""}°
+                      {units === "metric" ? "C" : "F"}
+                    </b>
+                  </h3>
+                  <hr />
+                  <h3 className="weather-description">
+                    Visibility: <b>{weather.visibility} m</b>
+                  </h3>
+                  <hr />
+                  <h3 className="weather-description">
+                    Wind Speed: <b>{weather.wind?.speed} mph</b>
+                  </h3>
                 </div>
-                <h2 className='weather-type'>{weather.weather?.[0]?.description}</h2>
-                <hr />
-                <h3 className='weather-description'>Humidity: <b>{weather.main?.humidity}%</b></h3>
-                <hr />
-                <h3 className='weather-description'>Feels like: <b>{weather.main ? Math.round(weather.main.feels_like) : ''}°{units === 'metric' ? 'C' : 'F'}</b></h3>
-                <hr />
-                <h3 className='weather-description'>Visibility: <b>{weather.visibility} m</b></h3>
-                <hr />
-                <h3 className='weather-description'>Wind Speed: <b>{weather.wind?.speed} mph</b></h3>
-            </div>
-        </div>
-    );
-};
+              </>
+            )}
+          </div>
+        );
+      }
+      
---
+++ src/Config/Config.js
@@ -0,0 +1,6 @@
+export const API = {
+    appId:'3e43c3d8b684e8223c23a336c725a765',
+    appIdB: "9bb575e18207e80b681cc765e5f1ad2e"
+}
+
+
